89d2704f7f718be6ea38694900e5cf5d6f0d5054
shahriar <shahriarksaurov@gmail.com>
2024-09-22 13:36:28 +0200

    update user, verify token and change password

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express';
 import User from '../models/User';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import { generateSecret } from '../utils/secretKey';
 import { generate2FToken } from '../utils/token2F';
 
 
+
+const generateToken = (userId: string) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+};
 
 
 // User Registration
@@ -25,7 +30,7 @@ export const register = async (req: Request, res: Response) => {
     } catch (error) {
       res.status(500).json({ message: 'Error registering user', error, success: false });
     }
-  };
+};
 
 
 // User Login with 2FA
@@ -57,4 +62,170 @@ export const login = async (req: Request, res: Response) => {
   }
 };
 
+// Verify 2FA and generate JWT
+export const verify2FA = async (req: Request, res: Response) => {
+  const { email, code } = req.body;
 
+  if (!email || !code) {
+    return res.status(400).json({ message: 'Email and 2FA code are required', success: false });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: 'User not found', success: false });
+
+    if (user.twoFactorCode !== code || new Date() > (user.twoFactorCodeExpiry!)) {
+      return res.status(400).json({ message: 'Invalid or expired 2FA code', success: false });
+    }
+
+    const token = generateToken(user._id.toString());
+    user.token = token;
+    user.twoFactorCode = '';
+    await user.save();
+    res.status(200).json({ message: 'Login successful', data:{
+        user: {
+            name: user.name,
+            email: user.email,
+            token: user.token,
+            mobile: user.mobile,
+            userId: user.userId
+        }
+    }, success: true });
+  } catch (error) {
+    res.status(500).json({ message: 'Error verifying 2FA', error, success: false });
+  }
+};
+
+// Update user details (name, email)
+export const updateUser = async (req: Request, res: Response) => {
+  
+  const { name, email } = req.body;
+
+  const userId = req.params.userId;
+
+  if (!name && !email) {
+    return res.status(400).json({ message: 'Name or email is required', success: false });
+  }
+
+  try {
+    const user = await User.findOne({ userId });
+    if (!user) return res.status(404).json({ message: 'User not found', success: false });
+    const updatedUser = await User.findOneAndUpdate({
+      userId: userId
+    }, {
+      name: name || user.name,
+      email: email || user.email
+    }, { new: true });
+    if (!updatedUser) return res.status(404).json({ message: 'User not found', success: false });
+
+    res.status(200).json({ message: 'User updated successfully', data: {
+        user: {
+            name: updatedUser?.name,
+            email: updatedUser?.email,
+            mobile: updatedUser?.mobile
+        }
+    }, success: true });
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating user', error, success: false });
+  }
+};
+
+// Change Password with SMS verification
+export const changePassword = async (req: Request, res: Response) => {
+  const { currentPassword } = req.body;
+  const userId = req.params.userId;
+
+  if (!currentPassword ) {
+    return res.status(400).json({ message: 'Current password is required', success: false });
+  }
+
+  try {
+    const user = await User.findOne({ userId });
+    if (!user) return res.status(404).json({ message: 'User not found', success: false });
+
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid current password', success: false });
+
+    // Send 2FA code for password change verification
+    const secretKey = generateSecret();
+    const twoFactorCode = generate2FToken(secretKey);
+    user.twoFactorCode = twoFactorCode;
+    user.twoFactorCodeExpiry = new Date(Date.now() + 10 * 60 * 1000); // 10 min expiry
+    await user.save();
+
+
+    res.status(200).json({ message: '2FA code sent for password change', success: true });
+  } catch (error) {
+    res.status(500).json({ message: 'Error during password change', error, success: false });
+  }
+};
+
+// Verify password change and update password
+export const verifyPasswordChange = async (req: Request, res: Response) => {
+  const { code, newPassword } = req.body;
+
+  const userId = req.params.userId;
+
+  try {
+    const user = await User.findOne({ userId});
+    if (!user) return res.status(404).json({ message: 'User not found', success: false });
+
+    if (user.twoFactorCode !== code || new Date() > (user.twoFactorCodeExpiry!)) {
+      return res.status(400).json({ message: 'Invalid or expired 2FA code', success: false });
+    }
+
+    user.password = await bcrypt.hash(newPassword, 10);
+    await user.save();
+
+    res.status(200).json({ message: 'Password updated successfully', success: true });
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating password', error, success: false });
+  }
+};
+
+// forgot password with SMS verification
+export const forgotPassword = async (req: Request, res: Response) => {
+  const { email,mobile } = req.body;
+
+  if (!email || !mobile) {
+    return res.status(400).json({ message: 'Email or mobile is required', success: false });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: 'User not found', success: false });
+
+    // Send 2FA code for password change verification
+    const secretKey = generateSecret();
+    const twoFactorCode = generate2FToken(secretKey);
+    user.twoFactorCode = twoFactorCode;
+    user.twoFactorCodeExpiry = new Date(Date.now() + 10 * 60 * 1000); // 10 min expiry
+    await user.save();
+
+
+    res.status(200).json({ message: '2FA code sent for password change', success: true });
+  } catch (error) {
+    res.status(500).json({ message: 'Error during password change', error, success: false });
+  }
+}
+
+// Verify password change and update password for forgot password
+export const verifyPasswordChangeForgotPass = async (req: Request, res: Response) => {
+  const { code, newPassword,email } = req.body;
+
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: 'User not found', success: false });
+
+    if (user.twoFactorCode !== code || new Date() > (user.twoFactorCodeExpiry!)) {
+      return res.status(400).json({ message: 'Invalid or expired 2FA code', success: false });
+    }
+
+    user.password = await bcrypt.hash(newPassword, 10);
+    await user.save();
+
+    res.status(200).json({ message: 'Password updated successfully', success: true });
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating password', error, success: false });
+  }
+};
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import {
-  register,
-  login
+    register,
+    login,
+    verify2FA,
+    updateUser,
+    changePassword,
+    verifyPasswordChange,
+    forgotPassword,
+    verifyPasswordChangeForgotPass
 } from '../controllers/authController';
 
+import { verifyToken } from '../middlewares/verifyUser';
 
 
 
@@ -13,6 +20,18 @@ const router = express.Router();
 router.post('/register', register);
 
 router.post('/login', login);
+
+router.post('/verify-2fa', verify2FA);
+
+router.put('/update-user/:userId',verifyToken ,updateUser);
+
+router.post('/change-password/:userId', verifyToken,changePassword);
+
+router.post('/verify-password-change/:userId', verifyToken,verifyPasswordChange);
+
+router.post('/forgot-password',forgotPassword);
+
+router.post('/verify-password-change-forgot-pass', verifyPasswordChangeForgotPass);
 
 
 
